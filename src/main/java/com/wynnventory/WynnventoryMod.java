5c22621020e77e2fba5388166beb38e7e7acc549
Tim Killenberger <timkillenberger@gmail.com>
2024-08-13 17:11:19 +0200

    Implemented automatic updates

--- src/main/java/com/wynnventory/WynnventoryMod.java
+++ src/main/java/com/wynnventory/WynnventoryMod.java
@@ -2,19 +2,31 @@ package com.wynnventory;
 
 import com.wynnventory.handler.TooltipHandler;
 import net.fabricmc.api.ModInitializer;
-
+import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.ModContainer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Optional;
+
 public class WynnventoryMod implements ModInitializer {
 	public static final Logger LOGGER = LoggerFactory.getLogger("wynnventory");
+	public static final Optional<ModContainer> WYNNVENTORY_INSTANCE = FabricLoader.getInstance().getModContainer("wynnventory");
 
 	@Override
 	public void onInitialize() {
+		if (WYNNVENTORY_INSTANCE.isEmpty()) {
+			error("Could not find Wynnventory in Fabric Loader!");
+			return;
+		}
+
+		String currentVersion = WYNNVENTORY_INSTANCE.get().getMetadata().getVersion().getFriendlyString();
+
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-		LOGGER.info("Initialized WynnVentoryMod");
+		LOGGER.info("Initialized WynnVentoryMod with version {}", currentVersion);
+
 		TooltipHandler.registerTooltips();
 	}
 
