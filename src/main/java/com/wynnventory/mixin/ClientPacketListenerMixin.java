e43317ed94cac054965ff03ca5670b7a3bd9fd5b
Dominic Pfister <dominic.pfister@swissonline.ch>
2024-08-16 18:33:34 +0200

    fix: Optimized sending of market data

--- src/main/java/com/wynnventory/mixin/ClientPacketListenerMixin.java
+++ src/main/java/com/wynnventory/mixin/ClientPacketListenerMixin.java
@@ -11,6 +11,8 @@ import net.minecraft.client.multiplayer.CommonListenerCookie;
 import net.minecraft.network.Connection;
 import net.minecraft.network.protocol.game.ClientboundContainerSetSlotPacket;
 import net.minecraft.network.protocol.game.ClientboundLoginPacket;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -29,9 +31,9 @@ public abstract class ClientPacketListenerMixin extends ClientCommonPacketListen
             at = @At("RETURN")
     )
     private void handleContainerSetSlotPre(ClientboundContainerSetSlotPacket packet, CallbackInfo ci) {
-        ContainerSetSlotEvent event = new ContainerSetSlotEvent.Pre(packet.getContainerId(), packet.getStateId(), packet.getSlot(), packet.getItem());
-        MixinHelper.post(event);
-
+        Item item = packet.getItem().getItem();
+        if (packet.getContainerId() > 0) return;
+        if (item == Items.AIR || item == Items.COMPASS) return;
         API.sendTradeMarketResults(packet.getItem());
     }
 
