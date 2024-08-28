b845d1b1b2b31da669b03ba76f74eb296ba3256c
Tim Killenberger <timkillenberger@gmail.com>
2024-08-28 10:16:26 +0200

    fix: Fixed references to static fields and removed unused applyConfig method

--- src/main/java/com/wynnventory/api/WynnventoryScheduler.java
+++ src/main/java/com/wynnventory/api/WynnventoryScheduler.java
@@ -15,7 +15,7 @@ public class WynnventoryScheduler {
     private static final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
 
     public static void startScheduledTask() {
-        scheduler.scheduleAtFixedRate(WynnventoryScheduler::processMarketAndLootItems, 1, ConfigManager.SEND_DELAY_MINS, TimeUnit.MINUTES);
+        scheduler.scheduleAtFixedRate(WynnventoryScheduler::processMarketAndLootItems, 1, ConfigManager.WYNNVENTORY_CONFIG.getSendDelayMins(), TimeUnit.MINUTES);
         ClientLifecycleEvents.CLIENT_STOPPING.register(client -> {
             WynnventoryScheduler.stopScheduledTask();
         });
@@ -26,7 +26,7 @@ public class WynnventoryScheduler {
         scheduler.shutdown();
 
         try {
-            if (!scheduler.awaitTermination(ConfigManager.SEND_DELAY_MINS, TimeUnit.MINUTES)) {
+            if (!scheduler.awaitTermination(ConfigManager.WYNNVENTORY_CONFIG.getSendDelayMins(), TimeUnit.MINUTES)) {
                 scheduler.shutdownNow();
             }
         } catch (InterruptedException e) {
--- src/main/java/com/wynnventory/config/ConfigManager.java
+++ src/main/java/com/wynnventory/config/ConfigManager.java
@@ -66,7 +66,6 @@ public enum ConfigManager {
         }
 
         registerKeybinds();
-        applyConfig();
     }
 
     public void saveConfig() {
