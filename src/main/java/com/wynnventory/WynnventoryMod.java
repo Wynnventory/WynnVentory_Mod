43daeb6ab3c222bded7270713967380007d3a7c6
Dominic Pfister <dominic.pfister@swissonline.ch>
2024-08-29 00:21:23 +0200

    Added keybind to toggle tooltip

--- src/main/java/com/wynnventory/WynnventoryMod.java
+++ src/main/java/com/wynnventory/WynnventoryMod.java
@@ -1,14 +1,23 @@
 package com.wynnventory;
 
+import com.mojang.blaze3d.platform.InputConstants;
 import com.sun.tools.javac.Main;
 import com.wynnventory.api.WynnventoryScheduler;
+import com.wynnventory.util.KeyMappingUtil;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.loader.api.FabricLoader;
 import net.fabricmc.loader.api.ModContainer;
+import net.minecraft.client.KeyMapping;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.ToggleKeyMapping;
+import org.lwjgl.glfw.GLFW;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Objects;
 import java.util.Optional;
 
 public class WynnventoryMod implements ModInitializer {
@@ -16,6 +25,9 @@ public class WynnventoryMod implements ModInitializer {
 	public static final Optional<ModContainer> WYNNVENTORY_INSTANCE = FabricLoader.getInstance().getModContainer("wynnventory");
 	public static String WYNNVENTORY_VERSION;
 	public static String WYNNVENTORY_MOD_NAME;
+
+	public static boolean SHOW_TOOLTIP = true;
+	private boolean keyPressed = false;
 
 	@Override
 	public void onInitialize() {
@@ -29,6 +41,28 @@ public class WynnventoryMod implements ModInitializer {
 		// Start WynnventoryScheduler
 		WynnventoryScheduler.startScheduledTask();
 
+		KeyMapping priceTooltipKey = KeyBindingHelper.registerKeyBinding(new KeyMapping(
+				"key.wynnventory.toggle_tooltip",
+				GLFW.GLFW_KEY_PERIOD,
+				"category.wynnventory.keybinding"
+		));
+
+		ClientTickEvents.END_CLIENT_TICK.register(client -> {
+			if (client.screen != null && client.player != null && KeyMappingUtil.getBoundKey(priceTooltipKey) != null) {
+				long windowHandle = Minecraft.getInstance().getWindow().getWindow();
+				int keyCode = Objects.requireNonNull(KeyMappingUtil.getBoundKey(priceTooltipKey)).getValue();
+
+				if (InputConstants.isKeyDown(windowHandle, keyCode)) {
+					System.out.println("CLICKED");
+					if (!keyPressed) {
+						SHOW_TOOLTIP = !SHOW_TOOLTIP;
+					}
+					keyPressed = true;
+				} else {
+					keyPressed = false;
+				}
+			}
+		});
 
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
