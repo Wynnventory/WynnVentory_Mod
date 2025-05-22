5aa2ba0d59391864a0d0d8bc38c00f47286469a3
Tim Killenberger <timkillenberger@gmail.com>
2025-05-22 16:44:03 +0200

    fix: fixed nullpointer exception when no lootpools were returned

--- src/main/java/com/wynnventory/model/item/RewardWeek.java
+++ src/main/java/com/wynnventory/model/item/RewardWeek.java
@@ -2,11 +2,12 @@ package com.wynnventory.model.item;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class RewardWeek {
-    private List<Lootpool> regions;
+    private List<Lootpool> regions = new ArrayList<>();
     private int week;
     private int year;
 
