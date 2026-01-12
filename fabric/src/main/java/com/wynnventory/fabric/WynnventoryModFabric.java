a5c2a1b51b92ab5c7c2e70bae322abc40c7470f0
Dominic Pfister <dominic.pfister@swissonline.ch>
2026-01-12 13:39:29 +0100

    ci: removed useless code

--- fabric/src/main/java/com/wynnventory/fabric/WynnventoryModFabric.java
+++ fabric/src/main/java/com/wynnventory/fabric/WynnventoryModFabric.java
@@ -1,18 +1,17 @@
 package com.wynnventory.fabric;
 
-import net.fabricmc.api.ModInitializer;
-
 import com.wynnventory.core.WynnventoryMod;
+import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
 import net.fabricmc.loader.api.ModContainer;
 
 import java.io.File;
 
-public final class WynnventoryModFabric implements ModInitializer {
+public final class WynnventoryModFabric implements ClientModInitializer {
     private static final ModContainer INSTANCE = FabricLoader.getInstance().getModContainer("wynnventory").orElseThrow(() -> new IllegalStateException("Wynnventory mod container not found"));
 
     @Override
-    public void onInitialize() {
+    public void onInitializeClient() {
         String version  = INSTANCE.getMetadata().getVersion().getFriendlyString();
         File modFile    = INSTANCE.getOrigin().getPaths().getFirst().toFile();
 
