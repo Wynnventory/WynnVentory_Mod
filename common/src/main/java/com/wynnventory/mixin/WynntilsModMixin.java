0883c1ccfe370489c81f46a43914aced191ce645
Dominic Pfister <dominic.pfister@swissonline.ch>
2026-01-21 14:54:09 +0100

    ci: removed unused imports

--- common/src/main/java/com/wynnventory/mixin/WynntilsModMixin.java
+++ common/src/main/java/com/wynnventory/mixin/WynntilsModMixin.java
@@ -1,22 +1,8 @@
 package com.wynnventory.mixin;
 
 import com.wynntils.core.WynntilsMod;
-import com.wynntils.utils.mc.McUtils;
-import com.wynnventory.core.WynnventoryMod;
-import com.wynnventory.event.RaidWindowOpenedEvent;
-import com.wynnventory.event.RewardPreviewOpenedEvent;
 import com.wynnventory.handler.TooltipRenderHandler;
-import com.wynnventory.model.container.RaidWindowContainer;
-import com.wynnventory.model.reward.RewardPool;
-import com.wynnventory.util.ContainerUtil;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.multiplayer.ClientCommonPacketListenerImpl;
-import net.minecraft.client.multiplayer.ClientPacketListener;
-import net.minecraft.client.multiplayer.CommonListenerCookie;
-import net.minecraft.network.Connection;
-import net.minecraft.network.protocol.game.ClientboundContainerSetContentPacket;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
