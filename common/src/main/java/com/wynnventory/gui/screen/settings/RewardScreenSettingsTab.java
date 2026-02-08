afc42f6e50642ededd3ef01e5ce8cd15e97a84d6
Dominic Pfister <dominic.pfister@swissonline.ch>
2026-02-08 14:57:41 +0100

    ci: formatting change

--- common/src/main/java/com/wynnventory/gui/screen/settings/RewardScreenSettingsTab.java
+++ common/src/main/java/com/wynnventory/gui/screen/settings/RewardScreenSettingsTab.java
@@ -8,6 +8,7 @@ import net.minecraft.network.chat.Component;
 import java.util.List;
 
 public class RewardScreenSettingsTab implements SettingsTab {
+
     @Override
     public List<OptionInstance<?>> getOptions() {
         RewardScreenSettings s = ModConfig.getInstance().getRewardScreenSettings();
