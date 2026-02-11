b81477442bdff6238681754beea01c284072b43a
Tim Killenberger <timkillenberger@gmail.com>
2026-02-11 13:33:30 +0100

    fix: added config for max notifier toasts

--- common/src/main/java/com/wynnventory/gui/screen/settings/NotificationSettingsTab.java
+++ common/src/main/java/com/wynnventory/gui/screen/settings/NotificationSettingsTab.java
@@ -3,6 +3,7 @@ package com.wynnventory.gui.screen.settings;
 import com.wynnventory.core.config.ModConfig;
 import com.wynnventory.core.config.settings.FavouriteNotifierSettings;
 import net.minecraft.client.OptionInstance;
+import net.minecraft.network.chat.Component;
 
 import java.util.List;
 
@@ -12,7 +13,15 @@ public class NotificationSettingsTab implements SettingsTab {
         FavouriteNotifierSettings s = ModConfig.getInstance().getFavouriteNotifierSettings();
         return List.of(
                 OptionInstance.createBoolean("gui.wynnventory.settings.notifications.enableNotifier", s.isEnableNotifier(), s::setEnableNotifier),
-                OptionInstance.createBoolean("gui.wynnventory.settings.notifications.mythicsOnly", s.isMythicsOnly(), s::setMythicsOnly)
+                OptionInstance.createBoolean("gui.wynnventory.settings.notifications.mythicsOnly", s.isMythicsOnly(), s::setMythicsOnly),
+                new OptionInstance<>(
+                        "gui.wynnventory.settings.notifications.maxToasts",
+                        OptionInstance.noTooltip(),
+                        (label, value) -> Component.literal(label.getString() + ": " + value),
+                        new OptionInstance.IntRange(1, 20),
+                        s.getMaxToasts(),
+                        s::setMaxToasts
+                )
         );
     }
 }
