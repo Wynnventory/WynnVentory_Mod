688bc3bdbd0357c5fad53c7a19ecd26ed79e3a40
Tim Killenberger <timkillenberger@gmail.com>
2026-02-12 15:33:17 +0100

    fix: ignoring mythic aspect helper method

--- common/src/main/java/com/wynnventory/model/reward/RewardPoolDocument.java
+++ common/src/main/java/com/wynnventory/model/reward/RewardPoolDocument.java
@@ -27,6 +27,8 @@ public final class RewardPoolDocument extends ModInfoProvider {
     }
 
     public List<SimpleItem> getItems() { return items; }
+
+    @JsonIgnore
     public List<SimpleItem> getMythicAspects() { return items.stream().filter(item -> item.getItemTypeEnum() == SimpleItemType.ASPECT && item.getRarityEnum() == GearTier.MYTHIC).toList(); }
     public String getRegion() { return rewardPool.getFullName(); }
     public String getType() { return rewardPool.getType().name(); }
