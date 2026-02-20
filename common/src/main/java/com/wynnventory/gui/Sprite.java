08abbfb2a937ca16489570ea27d945191f824808
Tim Killenberger <timkillenberger@gmail.com>
2026-02-20 17:46:08 +0100

    refactor: added mythic aspect display texture

--- common/src/main/java/com/wynnventory/gui/Sprite.java
+++ common/src/main/java/com/wynnventory/gui/Sprite.java
@@ -3,24 +3,25 @@ package com.wynnventory.gui;
 import net.minecraft.resources.Identifier;
 
 public enum Sprite {
-    RELOAD_BUTTON("gui/reload.png", 64, 32),
-    SETTINGS_BUTTON("gui/settings.png", 40, 20),
-    MYTHIC_ICON("gui/box_mythic.png", 16, 16),
-    FABLED_ICON("gui/box_fabled.png", 16, 16),
-    LEGENDARY_ICON("gui/box_legendary.png", 16, 16),
-    RARE_ICON("gui/box_rare.png", 16, 16),
-    UNIQUE_ICON("gui/box_unique.png", 16, 16),
-    COMMON_ICON("gui/box_normal.png", 16, 16),
-    SET_ICON("gui/box_set.png", 16, 16),
-    CHEST_SLOT("gui/chest_slot.png", 18, 18),
-    LOOTRUN_POOL_TOP_SECTION("gui/lootrun_pool_top_section.png", 208, 69),
-    RAID_POOL_TOP_SECTION("gui/raid_pool_top_section.png", 208, 69),
-    POOL_MIDDLE_SECTION_HEADER("gui/pool_middle_section_header.png", 176, 41),
-    POOL_MIDDLE_SECTION("gui/pool_middle_section.png", 176, 22),
-    POOL_BOTTOM_SECTION("gui/pool_bottom_section.png", 176, 13),
-    FILTER_SECTION("gui/filter.png", 105, 58),
-    ARROW_LEFT("gui/arrow_left.png", 64, 32),
-    ARROW_RIGHT("gui/arrow_right.png", 64, 32);
+    RELOAD_BUTTON("gui/rewardscreenrewardscreenreload.png", 64, 32),
+    SETTINGS_BUTTON("gui/rewardscreensettings.png", 40, 20),
+    MYTHIC_ICON("gui/rewardscreenbox_mythic.png", 16, 16),
+    FABLED_ICON("gui/rewardscreenbox_fabled.png", 16, 16),
+    LEGENDARY_ICON("gui/rewardscreenbox_legendary.png", 16, 16),
+    RARE_ICON("gui/rewardscreenbox_rare.png", 16, 16),
+    UNIQUE_ICON("gui/rewardscreenbox_unique.png", 16, 16),
+    COMMON_ICON("gui/rewardscreenbox_normal.png", 16, 16),
+    SET_ICON("gui/rewardscreenbox_set.png", 16, 16),
+    CHEST_SLOT("gui/rewardscreenchest_slot.png", 18, 18),
+    LOOTRUN_POOL_TOP_SECTION("gui/rewardscreenlootrun_pool_top_section.png", 208, 69),
+    RAID_POOL_TOP_SECTION("gui/rewardscreenraid_pool_top_section.png", 208, 69),
+    POOL_MIDDLE_SECTION_HEADER("gui/rewardscreenpool_middle_section_header.png", 176, 41),
+    POOL_MIDDLE_SECTION("gui/rewardscreenpool_middle_section.png", 176, 22),
+    POOL_BOTTOM_SECTION("gui/rewardscreenpool_bottom_section.png", 176, 13),
+    FILTER_SECTION("gui/rewardscreenfilter.png", 105, 58),
+    ARROW_LEFT("gui/rewardscreenarrow_left.png", 64, 32),
+    ARROW_RIGHT("gui/rewardscreenarrow_right.png", 64, 32),
+    MYTHIC_ASPECT_DISPALY("gui/raidlobby/mythic_aspect_display.png", 69, 102);
 
     private final Identifier resource;
     private final int width;
