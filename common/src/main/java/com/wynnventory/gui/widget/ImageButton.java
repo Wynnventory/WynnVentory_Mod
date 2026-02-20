37f9b32cefde3358d1308bc9cc0f0f72204afda1
Dominic Pfister <dominic.pfister@swissonline.ch>
2026-02-20 13:45:31 +0100

    refactor: removed comment

--- common/src/main/java/com/wynnventory/gui/widget/ImageButton.java
+++ common/src/main/java/com/wynnventory/gui/widget/ImageButton.java
@@ -24,8 +24,7 @@ public class ImageButton extends WynnventoryButton {
 
     @Override
     public void onPress(InputWithModifiers input) {
-        this.onPress.onPress(
-                null); // Button.OnPress expects a Button, but we are a WynntilsButton (which is an AbstractButton)
+        this.onPress.onPress(null);
     }
 
     @Override
