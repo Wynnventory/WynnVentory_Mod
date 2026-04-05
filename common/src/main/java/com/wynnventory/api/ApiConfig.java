4601d8983473a2d0147a88f13a5f0940543e678b
Dominic Pfister <dominic.pfister@swissonline.ch>
2026-04-05 22:24:16 +0200

    refactor: Updated dev backend URL

--- common/src/main/java/com/wynnventory/api/ApiConfig.java
+++ common/src/main/java/com/wynnventory/api/ApiConfig.java
@@ -10,7 +10,7 @@ import java.util.Base64;
 
 public final class ApiConfig {
     private static final String PROD_BASE = "https://www.wynnventory.com/api/";
-    private static final String DEV_BASE = "https://wynn-ventory-dev-2a243523ab77.herokuapp.com/api/";
+    private static final String DEV_BASE = "https://www.beta.wynnventory.com/api";
 
     private static final URI PROD_URI = URI.create(PROD_BASE);
     private static final URI DEV_URI = URI.create(DEV_BASE);
