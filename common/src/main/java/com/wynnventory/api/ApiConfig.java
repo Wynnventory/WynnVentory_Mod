88fe84ce9b665738ab9d675fe7c3a5b251ad7f77
Dominic Pfister <dominic.pfister@swissonline.ch>
2026-04-06 16:15:03 +0200

    refactor: Updated dev API url

--- common/src/main/java/com/wynnventory/api/ApiConfig.java
+++ common/src/main/java/com/wynnventory/api/ApiConfig.java
@@ -10,7 +10,7 @@ import java.util.Base64;
 
 public final class ApiConfig {
     private static final String PROD_BASE = "https://www.wynnventory.com/api/";
-    private static final String DEV_BASE = "https://www.beta.wynnventory.com/api";
+    private static final String DEV_BASE = "https://beta.wynnventory.com/api/";
 
     private static final URI PROD_URI = URI.create(PROD_BASE);
     private static final URI DEV_URI = URI.create(DEV_BASE);
