a7a0d385f4a00aa3fc5a9cebbc04c4dd0620786a
Dominic Pfister <dominic.pfister@swissonline.ch>
2026-04-14 23:02:20 +0200

    fix: Raid tooltip not displaying on every party finder tab

--- common/src/main/java/com/wynnventory/model/container/PartyFinderContainer.java
+++ common/src/main/java/com/wynnventory/model/container/PartyFinderContainer.java
@@ -3,12 +3,13 @@ package com.wynnventory.model.container;
 import java.util.regex.Pattern;
 
 public class PartyFinderContainer {
-    public static final Pattern TITLE = Pattern.compile("\uDAFF\uDFE4\uE03E");
+    public static final Pattern JOIN_TAB = Pattern.compile("\uDAFF\uDFE4\uE03E");
+    public static final Pattern QUEUE_TAB = Pattern.compile("\uDAFF\uDFE4\uE03F");
 
     private PartyFinderContainer() {}
 
     public static boolean matchesTitle(String title) {
         if (title == null) return false;
-        return TITLE.matcher(title).find();
+        return JOIN_TAB.matcher(title).find() || QUEUE_TAB.matcher(title).find();
     }
 }
