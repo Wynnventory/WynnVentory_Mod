76a1046b07238857aba6217287c10e5f6ff2f178
Tim Killenberger <timkillenberger@gmail.com>
2026-04-30 10:40:49 +0200

    refactor: changed stat order in tooltips

--- common/src/main/java/com/wynnventory/model/item/trademarket/PriceType.java
+++ common/src/main/java/com/wynnventory/model/item/trademarket/PriceType.java
@@ -5,38 +5,22 @@ import java.util.function.Function;
 import java.util.function.Predicate;
 
 public enum PriceType {
-    AVG_80(
-            "feature.wynnventory.tooltip.avg80",
-            TooltipSettings::isShowAverage80Price,
-            TrademarketItemSummary::getAverageMid80PercentPrice),
-    UNID_AVG_80(
-            "feature.wynnventory.tooltip.unidAvg80",
-            TooltipSettings::isShowUnidAverage80Price,
-            TrademarketItemSummary::getUnidentifiedAverageMid80PercentPrice),
     AVG(
             "feature.wynnventory.tooltip.avg",
             TooltipSettings::isShowAveragePrice,
             TrademarketItemSummary::getAveragePrice),
-    UNID_AVG(
-            "feature.wynnventory.tooltip.unidAvg",
-            TooltipSettings::isShowUnidAveragePrice,
-            TrademarketItemSummary::getUnidentifiedAveragePrice),
     HIGHEST(
             "feature.wynnventory.tooltip.highest",
             TooltipSettings::isShowMaxPrice,
             s -> s.getHighestPrice() == null ? null : (double) s.getHighestPrice()),
-    UNID_HIGHEST(
-            "feature.wynnventory.tooltip.unidHighest",
-            TooltipSettings::isShowUnidentifiedMaxPrice,
-            s -> s.getUnidentifiedHighestPrice() == null ? null : (double) s.getUnidentifiedHighestPrice()),
     LOWEST(
             "feature.wynnventory.tooltip.lowest",
             TooltipSettings::isShowMinPrice,
             s -> s.getLowestPrice() == null ? null : (double) s.getLowestPrice()),
-    UNID_LOWEST(
-            "feature.wynnventory.tooltip.unidLowest",
-            TooltipSettings::isShowUnidentifiedMinPrice,
-            s -> s.getUnidentifiedLowestPrice() == null ? null : (double) s.getUnidentifiedLowestPrice()),
+    AVG_80(
+            "feature.wynnventory.tooltip.avg80",
+            TooltipSettings::isShowAverage80Price,
+            TrademarketItemSummary::getAverageMid80PercentPrice),
     MEDIAN(
             "feature.wynnventory.tooltip.median",
             TooltipSettings::isShowMedian,
@@ -45,6 +29,22 @@ public enum PriceType {
             "feature.wynnventory.tooltip.movingMedian",
             TooltipSettings::isShowMovingMedian,
             s -> s.getMovingMedian() == null ? null : (double) s.getMovingMedian()),
+    UNID_AVG(
+            "feature.wynnventory.tooltip.unidAvg",
+            TooltipSettings::isShowUnidAveragePrice,
+            TrademarketItemSummary::getUnidentifiedAveragePrice),
+    UNID_HIGHEST(
+            "feature.wynnventory.tooltip.unidHighest",
+            TooltipSettings::isShowUnidentifiedMaxPrice,
+            s -> s.getUnidentifiedHighestPrice() == null ? null : (double) s.getUnidentifiedHighestPrice()),
+    UNID_LOWEST(
+            "feature.wynnventory.tooltip.unidLowest",
+            TooltipSettings::isShowUnidentifiedMinPrice,
+            s -> s.getUnidentifiedLowestPrice() == null ? null : (double) s.getUnidentifiedLowestPrice()),
+    UNID_AVG_80(
+            "feature.wynnventory.tooltip.unidAvg80",
+            TooltipSettings::isShowUnidAverage80Price,
+            TrademarketItemSummary::getUnidentifiedAverageMid80PercentPrice),
     UNID_MEDIAN(
             "feature.wynnventory.tooltip.unidMedian",
             TooltipSettings::isShowUnidMedian,
